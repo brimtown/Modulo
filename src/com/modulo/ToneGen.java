3d1f8a878ab77fabea2922e56b40658c6a142807
Tim Brown <brown.tim.lee@gmail.com>
2013-01-14 19:41:49 -0600

    added full project code

--- src/com/modulo/ToneGen.java
+++ src/com/modulo/ToneGen.java
@@ -1,22 +1,3 @@
-/*
-This file is part of Modulo.
-
-    Modulo is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Modulo is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Modulo.  If not, see <http://www.gnu.org/licenses/>.
-
-Copyright 2012-2013 Tim Brown
-*/
-
 package com.modulo;
 
 import android.app.Activity;
