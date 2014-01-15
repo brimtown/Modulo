d9a4f89408f9ce2b6ba0c75ecee3d802842767f3
Tim Brown <brown.tim.lee@gmail.com>
2014-01-15 17:36:44 -0600

    refactored code, removed excessive comments

--- src/com/modulo/Oscillator.java
+++ src/com/modulo/Oscillator.java
@@ -4,8 +4,205 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 
-public class Oscillator
-{
+public class Oscillator {
+
+	public Oscillator() {
+		// 440hz = "concert A", standard pitch
+		frequency = 440;
+		volume = 50;
+		isPlaying = false;
+		bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+				AudioFormat.ENCODING_PCM_16BIT);
+
+		buffer = new short[bufferSize];
+		sample = new short[sampleRate];
+
+		audioTrack = initializeAudioTrack();
+
+		// Waveform arrays
+		sqr = new short[sampleRate];
+		sin = new short[sampleRate];
+		tri = new short[sampleRate];
+		saw = new short[sampleRate];
+
+		// Pre-generates the lookup tables for each waveform, allows instant
+		// waveform switching
+		sqr = generateSqr();
+		sin = generateSin();
+		tri = generateTri();
+		saw = generateSaw();
+
+		// Sine wave is default
+		sample = sin;
+	}
+
+	public void pause() {
+		// Checks if audioTrack is initialized and if so, immediately pauses and
+		// flushes the audio data
+		if (audioTrack.getState() == AudioTrack.STATE_INITIALIZED) {
+			setIsPlaying(false);
+			audioTrack.pause();
+			audioTrack.flush();
+		}
+	}
+
+	// Sets up an AudioTrack
+	public AudioTrack initializeAudioTrack() {
+		AudioTrack returnTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+				sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+				AudioFormat.ENCODING_PCM_16BIT, buffer.length,
+				AudioTrack.MODE_STREAM);
+		Float scaled = (float)(volume/100.0f);
+		returnTrack.setStereoVolume(AudioTrack.getMaxVolume() * scaled, AudioTrack.getMaxVolume() * scaled);
+
+		return returnTrack;
+	}
+
+	public void play() {
+		if (audioTrack.getState() != AudioTrack.STATE_INITIALIZED) {
+			audioTrack = initializeAudioTrack();
+		}
+
+		setIsPlaying(true);
+		fillBuffer();
+		audioTrack.play();
+	}
+
+	public void stop() {
+		if (audioTrack.getState() == AudioTrack.STATE_INITIALIZED) {
+			setIsPlaying(false);
+			audioTrack.pause();
+			audioTrack.flush();
+			audioTrack.release();
+		}
+	}
+
+	public void setFreq(int freq) {
+		frequency = freq;
+	}
+
+	public int getFreq() {
+		return frequency;
+	}
+
+	public void setIsPlaying(boolean boo) {
+		isPlaying = boo;
+	}
+
+	public boolean getIsPlaying() {
+		return isPlaying;
+	}
+
+	public void setVolume(int vol) {
+		volume = vol;
+		Float scaled = (float) (volume / 100.0f);
+		audioTrack.setStereoVolume(AudioTrack.getMaxVolume() * scaled, AudioTrack.getMaxVolume() * scaled);
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	// Generates the sine wave lookup table
+	private short[] generateSin() {
+		short[] returnArray = new short[sampleRate];
+		for (int i = 0; i < sampleRate; i++) {
+			returnArray[i] = (short) ((float) Short.MIN_VALUE * (Math.sin((2.0f * (float) Math.PI * ((float) i))
+					/ ((float) sampleRate))));
+		}
+		return returnArray;
+	}
+
+	// Generates the sawtooth wave lookup table
+	private short[] generateSaw() {
+		short[] returnArray = new short[sampleRate];
+		for (int i = 0; i < sampleRate; i++) {
+			returnArray[i] = (short) ((float) Short.MIN_VALUE + (((float) i * (2.0f * (float) Short.MAX_VALUE)) / (float) sampleRate));
+		}
+		return returnArray;
+	}
+
+	// Generates the triangle wave lookup table
+	private short[] generateTri() {
+		short[] returnArray = new short[sampleRate];
+		for (int i = 0; i < sampleRate; i++) {
+			// First quarter of the waveform lookup table
+			if (i < (sampleRate / 4)) {
+				returnArray[i] = (short) (((float) i * (4.0f * (float) Short.MAX_VALUE)) / (float) sampleRate);
+			}
+			// Last quarter of the waveform lookup table
+			else if (i > (3 * (sampleRate / 4))) {
+				returnArray[i] = (short) ((((float) i * (4.0f * (float) Short.MAX_VALUE)) / (float) sampleRate) - 2.0f * Short.MAX_VALUE);
+			}
+			// Middle two sections of the waveform lookup table
+			else {
+				returnArray[i] = (short) ((float) 2.0f * Short.MAX_VALUE - (((float) i * (4.0f * (float) Short.MAX_VALUE)) / (float) sampleRate));
+			}
+		}
+		return returnArray;
+	}
+
+	// Generates the square wave lookup table
+	private short[] generateSqr() {
+		short[] returnArray = new short[sampleRate];
+		// The square wave is represented by two values (Short.MAX_VALUE and
+		// Short.MIN_VALUE)
+		// with the max value for the first half of the waveform, and the min
+		// for the second
+		for (int i = 0; i < sampleRate; i++) {
+			if (i < (sampleRate / 2)) {
+				returnArray[i] = Short.MAX_VALUE;
+			}
+			else {
+				returnArray[i] = Short.MIN_VALUE;
+			}
+		}
+		return returnArray;
+	}
+
+	public void fillBuffer() {
+		for (int i = 0; i < buffer.length; i++) {
+			// Causes phase to wrap around to the front of the lookup table
+			// when it exceeds its length
+			phase %= sample.length;
+
+			// Sets the short at buffer[i] to the value found in the lookup
+			// table
+			// of the current waveform sample
+			buffer[i] = sample[phase];
+
+			// Increments the phase by the current frequency, allowing it to
+			// step
+			// through the table at the correct speed and resulting in a change
+			// of pitch
+			phase += frequency;
+		}
+
+		// Writes the buffer to the audioTrack
+		audioTrack.write(buffer, 0, buffer.length);
+	}
+
+	// Changes the waveform
+	public void setWave(int pos) {
+		switch (pos) {
+		case 0:
+			sample = sin;
+			break;
+		case 1:
+			sample = saw;
+			break;
+		case 2:
+			sample = sqr;
+			break;
+		case 3:
+			sample = tri;
+			break;
+		default:
+			sample = sin;
+			break;
+		}
+	}
+
 	private int frequency;
 	public static final int MAX_FREQUENCY = 20000;
 	public static final int MIN_FREQUENCY = 20;
@@ -16,224 +213,4 @@ public class Oscillator
 	private int bufferSize;
 	private short[] sample, buffer, sin, sqr, tri, saw;
 	private int phase;
-
-	public Oscillator()
-	{
-		//Sets the initial frequency to 440Hz, or "concert A"
-		frequency = 440;
-		volume = 50;
-		isPlaying = false;
-		bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-
-		//Initializes an array of shorts for the buffer and the currently loaded waveform
-		buffer = new short[bufferSize];
-		sample = new short[sampleRate];
-
-		audioTrack = initializeAudioTrack();
-
-		//Initializes an array of shorts for each individual waveform
-		sqr = new short[sampleRate];
-		sin = new short[sampleRate];
-		tri = new short[sampleRate];
-		saw = new short[sampleRate];
-
-		//Pre-generates the lookup tables for each waveform, allowing the user to switch
-		//between waveforms without significant delay
-		sqr = generateSqr();
-		sin = generateSin();
-		tri = generateTri();
-		saw = generateSaw();
-
-		//Sets sine as the initial waveform
-		sample = sin;
-	}
-
-	public void pause()
-	{
-		//Checks if audioTrack is initialized and if so, immediately pauses and flushes the audio data
-		if(audioTrack.getState() == AudioTrack.STATE_INITIALIZED)
-		{
-			setIsPlaying(false);
-			audioTrack.pause();
-			audioTrack.flush();
-		}
-	}
-
-	//Sets up an AudioTrack
-	public AudioTrack initializeAudioTrack()
-	{
-		AudioTrack returnTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-				sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-				AudioFormat.ENCODING_PCM_16BIT, buffer.length,
-				AudioTrack.MODE_STREAM);
-
-		returnTrack.setStereoVolume(AudioTrack.getMaxVolume()*(float)(volume/100.0f), AudioTrack.getMaxVolume()*(float)(volume/100.0f));
-
-		return returnTrack;
-	}
-
-	public void play()
-	{
-		if(audioTrack.getState() == AudioTrack.STATE_INITIALIZED)
-		{
-			setIsPlaying(true);
-			fillBuffer();
-			audioTrack.play();
-		}
-		else
-		{
-			audioTrack = initializeAudioTrack();
-			setIsPlaying(true);
-			fillBuffer();
-			audioTrack.play();
-		}
-	}
-
-	public void stop()
-	{
-		if(audioTrack.getState() == AudioTrack.STATE_INITIALIZED)
-		{
-			setIsPlaying(false);
-			audioTrack.pause();
-			audioTrack.flush();
-			audioTrack.release();
-		}
-	}
-	public void setFreq(int freq)
-	{
-		frequency = freq;
-	}
-	public int getFreq()
-	{
-		return frequency;
-	}
-	public void setIsPlaying(boolean boo)
-	{
-		isPlaying = boo;
-	}
-	public boolean getIsPlaying()
-	{
-		return isPlaying;
-	}
-	public void setVolume(int vol)
-	{
-		volume = vol;
-		audioTrack.setStereoVolume(AudioTrack.getMaxVolume()*(float)(volume/100.0f), AudioTrack.getMaxVolume()*(float)(volume/100.0f));
-	}
-	public int getVolume()
-	{
-		return volume;
-	}
-	
-	//Generates the sine wave lookup table
-	private short[] generateSin()
-	{
-		short[] returnArray = new short[sampleRate];
-		for(int i=0; i<sampleRate; i++)
-		{
-			returnArray[i]=(short)((float) Short.MIN_VALUE * (Math.sin((2.0f*(float)Math.PI*((float)i))/((float)sampleRate))));
-		}
-		return returnArray;
-	}
-	
-	//Generates the sawtooth wave lookup table
-	private short[] generateSaw()
-	{
-		short[] returnArray = new short[sampleRate];
-		for(int i=0; i<sampleRate; i++)
-		{
-			returnArray[i]=(short) ((float)Short.MIN_VALUE + (((float)i *(2.0f * (float)Short.MAX_VALUE))/(float) sampleRate));	
-		}
-		return returnArray;
-	}
-	
-	//Generates the triangle wave lookup table
-	private short[] generateTri()
-	{
-		short[] returnArray = new short[sampleRate];
-		
-		//Since a triangle wave is a piecewise function, it requires several 'if' statements
-		for(int i=0; i<sampleRate; i++)
-		{
-			//First quarter of the waveform lookup table
-			if(i<(sampleRate/4))
-			{
-				returnArray[i]=(short)(((float)i*(4.0f * (float)Short.MAX_VALUE))/(float) sampleRate);
-			}
-			
-			//Last quarter of the waveform lookup table
-			else if(i>(3* (sampleRate/4)))
-			{
-				returnArray[i]=(short)((((float)i *(4.0f * (float)Short.MAX_VALUE))/(float) sampleRate)-2.0f*Short.MAX_VALUE);
-			}
-			
-			//Middle two sections of the waveform lookup table
-			else
-			{
-				returnArray[i]= (short) ((float)2.0f*Short.MAX_VALUE-(((float)i *(4.0f * (float)Short.MAX_VALUE))/(float) sampleRate));
-			}
-		}
-		return returnArray;
-	}
-	
-	//Generates the square wave lookup table
-	private short[] generateSqr()
-	{
-		short[] returnArray = new short[sampleRate];
-		
-		//The square wave is represented by two values (Short.MAX_VALUE and Short.MIN_VALUE)
-		//with the max value for the first half of the waveform, and the min for the second
-		for(int i=0; i<sampleRate; i++)
-		{
-			if(i<(sampleRate/2))
-			{
-				returnArray[i]=Short.MAX_VALUE;
-			}
-			else
-			{
-				returnArray[i]=Short.MIN_VALUE;
-			}
-		}
-		return returnArray;
-	}
-
-	public void fillBuffer()
-	{
-		//For each location in the buffer
-		for(int i=0; i<buffer.length; i++)
-		{
-			//Causes phase to wrap around to the front of the lookup table
-			//when it exceeds its length
-			phase%=sample.length;
-			
-			//Sets the short at buffer[i] to the value found in the lookup table
-			//of the current waveform sample
-			buffer[i]=sample[phase];
-			
-			//Increments the phase by the current frequency, allowing it to step
-			//through the table at the correct speed and resulting in a change of pitch
-			phase += frequency;
-		}
-		
-		//Writes the buffer to the audioTrack
-		audioTrack.write(buffer, 0, buffer.length);
-	}
-
-	//Changes the waveform
-	public void setWave(int pos)
-	{
-		switch(pos)
-		{
-		case 0: sample = sin;
-		break;
-		case 1: sample = saw;
-		break;
-		case 2: sample = sqr;
-		break;
-		case 3: sample = tri;
-		break;
-		default: sample = sin;
-		break;
-		}
-	}
 }
--- src/com/modulo/ToneGen.java
+++ src/com/modulo/ToneGen.java
@@ -2,18 +2,18 @@ package com.modulo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.KeyEvent;
-import android.widget.*;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
 
 public class ToneGen extends Activity
 {
-	private EditText textHz, textVol;
-	private Oscillator osc;
-	private ToggleButton buttonToggle;
-	private SeekBar seekFreq, seekVol;
-	private Spinner spinWave;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -21,165 +21,154 @@ public class ToneGen extends Activity
 		setContentView(R.layout.main);
 
 		osc = new Oscillator();
-		
-		//View assignments and initial setup
-		textHz = (EditText)findViewById(R.id.text_hz);
-		textVol = (EditText)findViewById(R.id.text_vol);
-		
-		buttonToggle = (ToggleButton)findViewById(R.id.toggle_play);
-		
-		seekFreq = (SeekBar)findViewById(R.id.slider_freq);
-		//seekFreq starts at Oscillator.MIN_FREQUENCY, so subtract that from its max value
+
+		// View assignments and initial setup
+		textHz = (EditText) findViewById(R.id.text_hz);
+		textVol = (EditText) findViewById(R.id.text_vol);
+
+		buttonToggle = (ToggleButton) findViewById(R.id.toggle_play);
+
+		seekFreq = (SeekBar) findViewById(R.id.slider_freq);
+		// seekFreq starts at Oscillator.MIN_FREQUENCY, so subtract that from
+		// its max value
 		seekFreq.setMax(Oscillator.MAX_FREQUENCY - Oscillator.MIN_FREQUENCY);
-		//Sets the initial seekFreq progress
+		// Sets the initial seekFreq progress
 		seekFreq.setProgress(osc.getFreq() - Oscillator.MIN_FREQUENCY);
-		
-		seekVol = (SeekBar)findViewById(R.id.slider_vol);
+
+		seekVol = (SeekBar) findViewById(R.id.slider_vol);
 		seekVol.setMax(100);
 		seekVol.setProgress(50);
-		
-		spinWave = (Spinner)findViewById(R.id.spinner_wave);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.wave_choices, android.R.layout.simple_spinner_item);
+
+		spinWave = (Spinner) findViewById(R.id.spinner_wave);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.wave_choices,
+				android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinWave.setAdapter(adapter);
 	}
 
-
 	@Override
 	protected void onStart() {
 		super.onStart();
-		
-		//Updates the frequency once textHz loses focus
-		textHz.setOnFocusChangeListener(new View.OnFocusChangeListener() 
-		{          
+
+		// Updates the frequency once the textHz EditText loses focus
+		textHz.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 			public void onFocusChange(View v, boolean hasFocus) {
-				if(!hasFocus)
-				{
-					int freqValue = Integer.parseInt(((EditText)v).getText().toString());
-					//Prevents a value greater than Oscillator.MAX_FREQUENCY from being sent
-					if(freqValue > Oscillator.MAX_FREQUENCY)
-					{
+				if (!hasFocus) {
+					int freqValue = Integer.parseInt(((EditText) v).getText().toString());
+					if (freqValue > Oscillator.MAX_FREQUENCY) {
 						osc.setFreq(Oscillator.MAX_FREQUENCY);
 					}
-					else
-					{
+					else {
 						osc.setFreq(freqValue);
 					}
-					
-					//Since the SeekBar starts at Oscillator.MIN_FREQUENCY, it needs to be subtracted
-					//from the value (i.e. 20Hz gets set at 0 on seekFreq)
+
+					// Since the SeekBar starts at Oscillator.MIN_FREQUENCY, it
+					// needs to be subtracted
+					// from the value (i.e. 20Hz gets set at 0 on seekFreq)
 					seekFreq.setProgress(osc.getFreq() - Oscillator.MIN_FREQUENCY);
 				}
 			}
 		});
-		
-		/*Updates the volume from textVol once 'enter' has been pressed on the soft keyboard.
-		  Since textVol is the final editable view in the activity, the user will be pressing
-		  'enter' instead of 'next' like in the previous EditText. Therefore, it makes more sense
-		  to wait for 'enter' instead of a loss of focus.*/
-		textVol.setOnKeyListener(new View.OnKeyListener() 
-		{          
-			public boolean onKey(View v, int keyCode, KeyEvent event)
-			{
-				if(keyCode == KeyEvent.KEYCODE_ENTER)
-				{
-					//Prevents a value greater than 100 from being sent to the volume
-					int volValue = Integer.parseInt(((EditText)v).getText().toString());
-					if(volValue > 100)
-					{
+
+		// Updates the volume from textVol once 'enter' has been pressed on the
+		// soft keyboard.
+		textVol.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (keyCode == KeyEvent.KEYCODE_ENTER) {
+					// Prevents a value greater than 100 from being sent to the
+					// volume
+					String value = ((EditText) v).getText().toString();
+					int volValue = Integer.parseInt(value);
+					if (volValue > 100) {
 						osc.setVolume(100);
 					}
-					else
-					{
+					else {
 						osc.setVolume(volValue);
 					}
 					seekVol.setProgress(osc.getVolume());
 					return true;
 				}
-				else
-				{
+				else {
 					return false;
 				}
 			}
 		});
 
-		//Handles the pausing and playing of the oscillator from the ToggleButton
-		buttonToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-		{	
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-			{
-				if(buttonView.isChecked())
-				{
+		// Handles the pausing and playing of the oscillator from the
+		// ToggleButton
+		buttonToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (buttonView.isChecked()) {
 					osc.play();
-					new Thread(new Runnable()
-					{
-						public void run()
-						{
-							//Continuously fills the buffer while the oscillator is playing
-							while(osc.getIsPlaying())
-							{
+					new Thread(new Runnable() {
+						public void run() {
+							// Continuously fills the buffer while the
+							// oscillator is playing
+							while (osc.getIsPlaying()) {
 								osc.fillBuffer();
 							}
 						}
-					}).start();			
+					}).start();
 				}
-				else
-				{
+				else {
 					osc.pause();
 				}
 			}
 		});
 
-		//Updates the oscillator's frequency based on the progress of seekFreq
-		seekFreq.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-		{
-			public void onStopTrackingTouch(SeekBar seekBar)
-			{
+		// Updates the oscillator's frequency based on the progress of seekFreq
+		seekFreq.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			public void onStopTrackingTouch(SeekBar seekBar) {
 			}
-			public void onStartTrackingTouch(SeekBar seekBar)
-			{
+
+			public void onStartTrackingTouch(SeekBar seekBar) {
 			}
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-			{
+
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				osc.setFreq(Oscillator.MIN_FREQUENCY + seekBar.getProgress());
 				textHz.setText("" + osc.getFreq());
 			}
 		});
 
-		//Updates the oscillator's volume based on the progress of seekVol
+		// Updates the oscillator's volume based on the progress of seekVol
 		seekVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
 		{
 			public void onStopTrackingTouch(SeekBar seekBar)
 			{
 			}
+
 			public void onStartTrackingTouch(SeekBar seekBar)
 			{
 			}
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-			{
+
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				osc.setVolume(seekBar.getProgress());
 				textVol.setText("" + osc.getVolume());
 			}
 		});
 
-		//Updates the oscillator's waveform based on the selection in spinWave
-		spinWave.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-		{
-			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
-			{
+		// Updates the oscillator's waveform based on the selection in spinWave
+		spinWave.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 				osc.setWave(pos);
 			}
-			public void onNothingSelected(AdapterView<?> parent)
-			{
+
+			public void onNothingSelected(AdapterView<?> parent) {
 			}
 		});
 	}
 
-	//Stops the oscillator playback and releases the AudioTrack resources 
+	// Stops the oscillator playback and releases the AudioTrack resources
 	@Override
 	protected void onPause() {
 		super.onPause();
 		osc.stop();
 		buttonToggle.setChecked(false);
 	}
+
+	private EditText textHz, textVol;
+	private Oscillator osc;
+	private ToggleButton buttonToggle;
+	private SeekBar seekFreq, seekVol;
+	private Spinner spinWave;
 }
